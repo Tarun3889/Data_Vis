9cbf3f255ca4edf39b99b3bca8df3ce87aa05471
Tarun Kumar <39798981+Tarun3889@users.noreply.github.com>
2021-03-19 21:21:34 +0530

    2

--- Plot2.R
+++ Plot2.R
@@ -1,7 +1,20 @@
+## Aim of this function is to 
+## 1. read the household_power_consumption.txt file
+## 2. subset for data taken from 2 days: 2007-02-01 and 2007-02-02
+## 3. generate a histogram of global active power(kilowatts)
+
+## Parameters: none
+## Assumes household_power_consumption.txt file located in working dir
+
+## read data
 
 data <- read.table('household_power_consumption.txt', header=TRUE, sep=";", stringsAsFactors=FALSE)
+## subset data from 2007-02-01 and 2007-02-02
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+## change class of the Global_active_power column to correct class
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
